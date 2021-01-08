eababc1e80c67c67eb2f422d3335356245c3adf8
chadowBdf <adekambirachad@gmail.com>
2021-01-08 22:37:02 +0100

    commit

--- contact.php
+++ contact.php
@@ -1,3 +1,8 @@
 <?php
-
+if (isset($_POST) && isset ($_POST["user_name"]) && isset ($_POST["user_mail"]) && isset ($_POST["message"])){
+    extract($_POST);
+    if (!empty($user_name) && !empty($user_mail) && !empty($message)){
+        echo $message;
+    }
+}
 ?>
