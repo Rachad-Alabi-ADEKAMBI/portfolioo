b551f66cc0b5aecde64c16e0d1e1f8d4292a9b7a
chadowBdf <adekambirachad@gmail.com>
2021-01-13 10:46:13 +0100

    correction des erreurs et contact.php

--- contact.php
+++ contact.php
@@ -1,21 +1,24 @@
 <?php
-if (isset($_POST["submit"])){
-   if (isset($_POST["user_name"]) AND isset($_POST["user_mail"]) AND isset($_POST["subject"]) AND isset($_POST["user_message"]))
+if (isset($_POST) AND isset($_POST["user_name"]) AND isset($_POST["user_mail"]) AND isset($_POST["user_query"]) AND isset($_POST["user_message"]))
+{
    extract($_POST);
-   if (!empty($_POST["user_name"]) AND !empty($_POST["user_mail"]) AND !empty($_POST["subject"]) AND !empty($_POST["user_message"])){
-               $message = str_replace ("\'", "'", $user_message);
-               $destinataire = "adekambirachad@gmail.com";
-               $sujet = "formulaire de contact";
-               $message = "un nouveau message \n 
-               Nom: $user_name \n
-               Email: $user_mail \n
-               Message: $user_message";
-               $header = "from $user_mail \n Reply-To: $user_mail";
-                mail($destinataire, $sujet, $message, $header);
-                echo "Le mail a bien été envoyé !<br>";
-            }
-            else{
-               echo "Vous n'avez pas rempli tous les champs !";
-            }
+   if(!empty($user_name) AND !empty($user_mail) AND !empty($user_query) AND !empty($user_message))
+   {  
+      $user_message= str_replace("\'","'", $user_message);
+      $receiver= "adekambirachad@gmail.com";
+      $query="Formulaire de contact";
+      $message="Une nouvelle question \n
+      Nom: $user_name \n
+      Email: $user_mail \n
+      Objet: $user_query \n
+      Message: $user_message";
+      $heading="From: $user_name \n Reply-To $user_mail";
+      mail($receiver, $query, $message, $heading);
+      echo"Le mail a bien été envoyé.";
+   }
+   else{
+      echo"Vous n'avez pas rempli tous les champs !!!";
+   }
 }
+
 ?>
